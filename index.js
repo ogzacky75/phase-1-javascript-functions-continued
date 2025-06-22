1a20a17019cae7def72d9c84245c7f7f3226d62f
ogzacky75 <ogzacky75@gmail.com>
2025-06-22 09:59:30 +0300

    codegrade

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+
+function saturdayFun(action='roller-skate'){
+	return `This Saturday, I want to ${action}!`
+	
+}
+
+let mondayWork = function(action='go to the office'){
+	return `This Monday, I will ${action}.`
+}
+
+function wrapAdjective(highLight = "*"){
+	
+	return function(special="a dedicated programmer"){
+		return`You are ${highLight} ${special} ${highLight}!`
+
+		
+	}
+}
